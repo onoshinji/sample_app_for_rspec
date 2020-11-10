769a2cf5bab25aea61180ab1aecaf3b5e363e400
ono shinji <yjsnpi555@gmail.com>
2020-11-10 16:35:12 +0900

    ログインモジュールを作成、ファイルとして読み込む設定を追加

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -20,6 +20,8 @@ RSpec.configure do |config|
   config.infer_spec_type_from_file_location!
 
   config.filter_rails_from_backtrace!
+  Dir[Rails.root.join('spec', 'support', '**', '*.rb')].sort.each { |f| require f }
   config.include FactoryBot::Syntax::Methods
-
+  # ログイン機能のメソッドを使うため、クラス名を記載
+  config.include LoginMacros
 end
