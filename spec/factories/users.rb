e78d51168ab84e1e6a36cf6026e9e4a986a88b8f
ono shinji <yjsnpi555@gmail.com>
2020-11-10 17:09:44 +0900

    解答例を見て修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,6 @@
 FactoryBot.define do
   factory :user do
+    # ユーザーが作成されるたびに違うuser_(番号)で作成される
     sequence(:email) { |n| "user_#{n}@example.com" }
     password { "password" }
     password_confirmation { "password" }
